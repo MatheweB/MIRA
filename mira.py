2b8334e1fc673b1776bae4ac926c3a342fd79fcd
Mathewe Banda <bbanda@oberlin.edu>
2017-11-27 20:21:31 -0500

    Built basic file opening/parsing

--- mira.py
+++ mira.py
@@ -1,11 +1,16 @@
 from PIL import Image
+import os
+import sys
+import tensorflow as tf
+import math
 
 
 def buildNetwork(training, testing, labelNum, attributes, labels):
-    #Do networthy shit
+    #Do networthy stuff
     pass
 
 def subdivide(image,subNum):
+    pass
 
     #Decides how many subdivisions there are
 
@@ -22,18 +27,28 @@ def preProcessImages(images):
     
 
 def getLargestImage(images):
-    pass
+    largestImage = None
+    bestDim = 1
+    
+    for image in images:
+        size = image.size
+        x = size[0]
+        y = size[1]
+        diff = math.fabs(x-y)
+        if diff/(x+y) < bestDim:
+            bestDim = diff/(x+y)
+            largestImage = image
+
+    print(largestImage.size)
+    exit(5)
 
 
 def transformImages (images, size): #MAKE SURE TO HAVE DIMENSIONS BE A MULTIPLE OF 4
-
-    img = Image.open('image.png').convert('LA') #(A maintains Alpha Channel/Intensity)
     pass
     
     
-def main():
-
-    
+def main(images,num_neurons,learning_rate,training_runs,percentage,seed):
+                        
     preProcessImages(images)
     
     for image in images:
@@ -45,19 +60,28 @@ def main():
     
 
 
+
 if __name__ == "__main__":
-    path = sys.argv[1]  # Usually just the filename in the same folder
-    num_neurons = int(sys.argv[2])
-    learning_rate = float(sys.argv[3]) # K-Value (an integer)
-    training_runs = int(sys.argv[4])
-    percentage = float(sys.argv[5])  # Percentage of data to be used for TRAINING
-    seed = float(sys.argv[6])
+    num_neurons = 1#int(sys.argv[1])
+    learning_rate = 1#float(sys.argv[2]) # K-Value (an integer)
+    training_runs = 1#int(sys.argv[3])
+    percentage = 1#float(sys.argv[4])  # Percentage of data to be used for TRAINING
+    seed = 1#float(sys.argv[5])
 
     #OPEN FILES HERE
     images = [] #where an image is [image,label]
+    photoTypes = []
+    CWD = os.getcwd() + "/photos"
     
-    for image in thingy:
-        pass
+    for photoType in os.listdir(CWD): #Adds all photo type folders
+        photoTypes.append(photoType)
+        
+    
+    for photoType in photoTypes:
+        photoDir = CWD + "/" + photoType
+        for imageName in os.listdir(photoDir):
+            image = Image.open(imageName).convert('LA') #Greyscales and keeps alpha
+            images.append([image,photoType])
 
     #Possibly make cases for seed not being a string, etc...
     main(images,num_neurons,learning_rate,training_runs,percentage,seed)
