321dff982110afae06094b54da13b5dce269e344
Mathewe Banda <bbanda@oberlin.edu>
2017-12-01 16:03:55 -0500

    Separated pre-processing into pp.py. Quinn need to make test/training set separation, and vectorizing labels!

--- mira.py
+++ mira.py
@@ -1,114 +1,46 @@
 from PIL import Image
+from pp import preProcessImages, Instance
 import os
 import sys
 import tensorflow as tf
 import math
+import random
 
-def buildNetwork(training, testing, labelNum, attributes, labels):
+def buildNetwork(training, testing, numLabels, numSub, numPixels):
 
     #None = number of images
     #numSub = number of sub-images
     #numPixel = number of pixels in sub-image
-
-    x = tf.placeholder(tf.float32, shape = [None, numSub, numPixel])
+    
+    X = tf.placeholder(tf.float32, shape = [None, numSub, numPixel])
+    
+    Y = tf.placeholder(tf.float32, shape = [None, numLabels])
 
     #Layer 1
-    w_hidden_1 = tf.Variable(tf.truncated_normal[])
+    #w_hidden_1 = tf.Variable(tf.truncated_normal[])
 
 
     #Layer2
-    w_hidden_2
+    #w_hidden_2
 
 
     #Output Layer
-    w_hidden_3
+    #w_hidden_3
 
 
     #None = number of images
     #numlabels = number of labels (10 for our original set)
-    y = tf.placeholder(tf.float32, shape = [None, numSub, numLabels])
+    #y = tf.placeholder(tf.float32, shape = [None, numSub, numLabels])
 
     pass
 
-
-def subdivide(images,divisionNum,stepNum,imageSideSize): #squares go DOWN, not up
     
-    segNum = (imageSideSize/divisionNum)/stepNum #describes how much we should move over each time
-
-    divisionList = [] #List of steps for our algorithm
-
-    squareSize = (imageSideSize/divisionNum)
-    totalSize = (imageSideSize) - squareSize
-    currentSize = 0
-    divisionList.append(currentSize)
-    while currentSize < totalSize:
-        currentSize = currentSize + segNum
-        divisionList.append(currentSize)
-
-    for imageType in images:
-        for image in imageType:
-            subImageList = []
-            for xStep in divisionList:
-                for yStep in divisionList:
-                    subImage = image[0].crop((xStep,yStep,xStep+squareSize,yStep+squareSize))
-                    subImageList.append(list(subImage.getdata()))
-            image[0] = subImageList #changes all images into an array of sub-images    
+def main(instances,num_neurons,learning_rate,training_runs,percentage,divisionNum,stepNum):
     
+    trainingSet, testingSet = preProcessImages(instances,(stepNum*divisionNum), divisionNum, stepNum) #MAKE SURE THESE ARE RIGHT
 
-def preProcessImages(images,modNum):
-    imageSize = getLargestImage(images,modNum) #[x,y]
-    transformImages(images, imageSize)
-    return imageSize[0]
-
-def getLargestImage(images,modNum): #gets the largest, most square image
-    largestImage = None
-    bestDim = 1
+    buildNetwork(trainingSet, testingSet, len(instances), len(instances[0][0].subImages), len(instances[0][0].subImages[0]))
     
-    for imageType in images:
-        for image in imageType:
-            size = image[0].size
-            x = size[0]
-            y = size[1]
-            diff = math.fabs(x-y)
-            if diff == 0: #accounts for a perfect square
-                diff = 1
-            if diff/(x+y) < bestDim:
-                bestDim = diff/(x+y)
-                largestImage = image[0]
-
-    largestSize = largestImage.size
-    x = largestSize[0]
-    y = largestSize[1]
-    if x != y:
-        if x > y:
-            y = x
-        if y > x:
-            x = y
-            
-    while x%modNum != 0: #ensures the dimensions are easy to deal with in subdividing (have factors that work) 
-        x += 1
-
-    return [x,x]
-
-
-def transformImages (images, size): 
-    for imageType in images:
-        for image in imageType:
-            image[0] = image[0].resize(size, Image.ANTIALIAS)
-    
-    
-def main(images,num_neurons,learning_rate,training_runs,percentage,divisionNum,stepNum,seed):
-    
-    imageSideSize = preProcessImages(images,(stepNum*divisionNum))
-    subdivide(images,divisionNum,stepNum,imageSideSize)
-
-    print(len(images[0][0][0][0])) #imagetype,image,imagefile,1st sub-image, 1st pixel value
-    print(len(images[0][0][0][1]))
-
-    buildNetwork(trainingSet, testingSet, len(labels), attributes, labels)
-    
-
-
 
 if __name__ == "__main__":
     num_neurons = 1#int(sys.argv[1])
@@ -118,11 +50,11 @@ if __name__ == "__main__":
     divisionNum = 3#int(sys.argv[5]) #How many times we want to divide up the image into smaller squares
     stepNum = 2#int(sys.argv[6]) #Decides how much overlap we have per subdivision 
     seed = 1 #float(sys.argv[7])
-
     
     #Opens Images Here (Image files must be of type .jpg)
-    images = [] #where an image is [image,label]
-    photoTypes = []
+    instances = []
+    photoTypes = [] #list of "labels"
+    
     CWD = os.getcwd() + "/photos"
     
     for photoType in os.listdir(CWD): #Adds all photo type folders
@@ -135,14 +67,16 @@ if __name__ == "__main__":
         for imageName in os.listdir(photoDir):
             if ".jpg" in imageName:
                 image = Image.open(photoDir +"/"+ imageName).convert('L') #Greyscales
-                photoTypeList.append([image,photoType])
+                instance = Instance()
+                instance.sLabel = photoType
+                instance.image = image
+                photoTypeList.append(instance)
                 
-        images.append(photoTypeList)
+        instances.append(photoTypeList)
+
+    random.seed(seed)
                 
-    main(images,num_neurons,learning_rate,training_runs,percentage,divisionNum,stepNum,seed)
-     
-    
-    
+    main(instances,num_neurons,learning_rate,training_runs,percentage,divisionNum,stepNum)
 
     
     
---
+++ pp.py
@@ -0,0 +1,82 @@
+from PIL import Image
+import math
+import random
+
+class Instance:
+    def __init__(self):
+        self.sLabel = ""
+        self.vLabel = []
+        self.image = None
+        self.subImages = []
+
+def subdivide(instances,divisionNum,stepNum,imageSideSize): #squares go DOWN, not up
+    
+    segNum = (imageSideSize/divisionNum)/stepNum #describes how much we should move over each time
+
+    divisionList = [] #List of steps for our algorithm
+
+    squareSize = (imageSideSize/divisionNum)
+    totalSize = (imageSideSize) - squareSize
+    currentSize = 0
+    divisionList.append(currentSize)
+    while currentSize < totalSize:
+        currentSize = currentSize + segNum
+        divisionList.append(currentSize)
+
+    for instanceType in instances:
+        for instance in instanceType:
+            subImageList = []
+            for xStep in divisionList:
+                for yStep in divisionList:
+                    subImage = instance.image.crop((xStep,yStep,xStep+squareSize,yStep+squareSize))
+                    subImageList.append(list(subImage.getdata()))
+            instance.subImages = subImageList #changes all images into an array of sub-images    
+    
+
+def preProcessImages(instances,modNum, divisionNum, stepNum):
+    imageSize = getLargestImage(instances,modNum) #[x,y]
+    imageSideSize = imageSize[0]
+    
+    transformImages(instances, imageSize)
+    subdivide(instances,divisionNum,stepNum,imageSideSize)
+
+    exit(5)
+    
+    vectorizeLabels(instances) #QUINN TO WRITE THESE
+    return splitSets(instances)
+
+def getLargestImage(instances,modNum): #gets the largest, most square image
+    largestImage = None
+    bestDim = 1
+    
+    for instanceType in instances:
+        for instance in instanceType:
+            size = instance.image.size
+            x = size[0]
+            y = size[1]
+            diff = math.fabs(x-y)
+            if diff == 0: #accounts for a perfect square
+                diff = 1
+            if diff/(x+y) < bestDim:
+                bestDim = diff/(x+y)
+                largestImage = instance.image
+
+    largestSize = largestImage.size
+    x = largestSize[0]
+    y = largestSize[1]
+    if x != y and y > x:
+        x = y
+            
+    while x%modNum != 0: #ensures the dimensions are easy to deal with in subdividing (have factors that work) 
+        x += 1
+
+    return [x,x]
+
+
+def transformImages (instances, size): 
+    for instanceType in instances:
+        for instance in instanceType:
+            instance.image = instance.image.resize(size, Image.ANTIALIAS)
+
+    
+    
